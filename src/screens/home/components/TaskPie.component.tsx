a2d3d14ab60d72db33c103c306d4ab792cc133d7
pwc <pengwencheng@zhichongjia.com>
2020-12-14 17:26:55 +0800

    首页新增图表

---
+++ src/screens/home/components/TaskPie.component.tsx
@@ -0,0 +1,162 @@
+import {pBackgroundImageStyles} from '@/styles/background-image';
+import pxToDp from '@/utils/pxToDp';
+import React from 'react';
+import {
+  Dimensions,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import Svg from 'react-native-svg';
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryLabel,
+  VictoryLegend,
+  VictoryPie,
+  VictoryScatter,
+  VictoryTheme,
+} from 'victory-native';
+
+const TaskPie = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('../../../assets/images/home/img_Dashbiard_background.png')}
+        style={[styles.main]}>
+        <View style={styles.pieWrap}>
+          <VictoryPie
+            width={pxToDp(200)}
+            height={pxToDp(200)}
+            theme={VictoryTheme.material}
+            labels={() => null}
+            data={[
+              {x: 'Draft', y: 0},
+              {x: 'In Progress', y: 50},
+              {x: 'Ready for Submit', y: 25},
+              {x: 'Overdue', y: 25},
+            ]}
+            radius={pxToDp(80)}
+            innerRadius={pxToDp(100)}
+          />
+          <View style={styles.pieLabel}>
+            <Text style={{fontSize: pxToDp(48)}}>4</Text>
+            <Text style={{fontSize: pxToDp(20)}}>Total Forms</Text>
+          </View>
+        </View>
+
+        <View style={styles.echartLabel}>
+          <View style={styles.echartLabelItem}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <View
+                style={[
+                  styles.echartLabelItemMark,
+                  {backgroundColor: '#707070'},
+                ]}></View>
+              <Text>Draft</Text>
+            </View>
+            <Text>0%</Text>
+          </View>
+          <View style={styles.echartLabelItem}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <View
+                style={[
+                  styles.echartLabelItemMark,
+                  {backgroundColor: '#0069A6'},
+                ]}></View>
+              <Text>In Progress</Text>
+            </View>
+            <Text>50%</Text>
+          </View>
+          <View style={styles.echartLabelItem}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <View
+                style={[
+                  styles.echartLabelItemMark,
+                  {backgroundColor: '#588309'},
+                ]}></View>
+              <Text>Ready for Submit</Text>
+            </View>
+            <Text>25%</Text>
+          </View>
+          <View style={styles.echartLabelItem}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <View
+                style={[
+                  styles.echartLabelItemMark,
+                  {backgroundColor: '#DE464C'},
+                ]}></View>
+              <Text>Overdue</Text>
+            </View>
+            <Text>25%</Text>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: pxToDp(320),
+  },
+  main: {
+    flex: 1,
+    height: pxToDp(320),
+    paddingHorizontal: pxToDp(30),
+    paddingVertical: pxToDp(60),
+    backgroundColor: '#FFF',
+    borderRadius: 6,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  pieWrap: {
+    position: 'relative',
+    width: pxToDp(200),
+    height: pxToDp(200),
+  },
+  pieLabel: {
+    position: 'absolute',
+    width: '100%',
+    height: pxToDp(160),
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  echartLabel: {
+    flex: 1,
+    height: '100%',
+    marginLeft: pxToDp(32),
+    justifyContent: 'space-between',
+  },
+  echartLabelItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  echartLabelItemMark: {
+    width: pxToDp(16),
+    height: pxToDp(16),
+    marginRight: pxToDp(24),
+  },
+});
+
+export default TaskPie;
--- src/screens/home/home.component.tsx
+++ src/screens/home/home.component.tsx
@@ -1,53 +1,70 @@
-
 import React from 'react';
-import { ImageBackground, Pressable, StyleSheet, Text, View, Image } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  ImageBackground,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import TaskSwiper from './components/TaskSwiper.component';
-import { pBackgroundImageStyles } from '@/styles/background-image';
-import { pColorStyles } from '@/styles/color';
-import { pFontStyles } from '@/styles/font';
+import {pBackgroundImageStyles} from '@/styles/background-image';
+import {pColorStyles} from '@/styles/color';
+import {pFontStyles} from '@/styles/font';
 import pxToDp from '@/utils/pxToDp';
+import TaskPie from './components/TaskPie.component';
 
-
-
-export const HomeScreen = ({ navigation }: any) => {
-
+export const HomeScreen = ({navigation}: any) => {
   const goSignIn = () => {
-    navigation.navigate('AuthLayout', { screen: 'SignIn' });
+    navigation.navigate('AuthLayout', {screen: 'SignIn'});
   };
 
   return (
-    <ImageBackground source={require('../../assets/images/home/img_landing_background.png')} style={pBackgroundImageStyles.fullBg}>
+    <ImageBackground
+      source={require('../../assets/images/home/img_landing_background.png')}
+      style={pBackgroundImageStyles.fullBg}>
       <SafeAreaView style={styles.containter}>
         <View>
           <Pressable style={styles.setting}>
-            <Image source={require('../../assets/images/icons/icon_settings.png')}></Image>
+            <Image
+              source={require('../../assets/images/icons/icon_settings.png')}></Image>
           </Pressable>
           <View>
             <Text style={pColorStyles.grey}>Welcome back to</Text>
-            <Pressable style={styles.projectBox}>
-              <Text style={[pFontStyles.weightBold, styles.project]}>J3668-Development Project </Text>
-              <Image source={require('../../assets/images/icons/icon_drop_down.png')}></Image>
+            <Pressable style={styles.projectBox} onPress={goSignIn}>
+              <Text style={[pFontStyles.weightBold, styles.project]}>
+                J3668-Development Project{' '}
+              </Text>
+              <Image
+                source={require('../../assets/images/icons/icon_drop_down.png')}></Image>
             </Pressable>
           </View>
         </View>
-        <View>
+        <View style={styles.taskSwiper}>
           <TaskSwiper></TaskSwiper>
         </View>
-     
+        <View>
+          <Text style={[styles.title, pFontStyles.weightBold]}>My Tasks</Text>
+          <TaskPie></TaskPie>
+        </View>
       </SafeAreaView>
     </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  title: {
+    fontSize: pxToDp(34),
+    marginBottom: pxToDp(24),
+  },
   containter: {
     paddingVertical: pxToDp(16),
-    paddingHorizontal: pxToDp(32)
+    paddingHorizontal: pxToDp(32),
   },
   setting: {
     alignItems: 'flex-end',
-    marginBottom: pxToDp(20)
+    marginBottom: pxToDp(20),
   },
   projectBox: {
     marginTop: pxToDp(14),
@@ -55,9 +72,10 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     borderBottomWidth: 1,
-    borderBottomColor: '#DFE8F0'
+    borderBottomColor: '#DFE8F0',
   },
   project: {
-    fontSize: pxToDp(36)
+    fontSize: pxToDp(36),
   },
-})
+  taskSwiper: {},
+});
